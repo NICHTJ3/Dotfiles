c2deb33640952f84ddadde8f8bfa8a11f0ae7bf5
NICHTJ3 <nicholsontrent@gmail.com>
2020-10-01 22:06:37 +1300

    Add linux related config and install conf files

---
+++ linux/config/wm-scripts/bar-functions/battery.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# A dwm_bar function to read the battery level and status
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+dwm_battery () {
+    # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
+    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT0/status)
+
+    printf "%s" "$SEP1"
+    if [ "$STATUS" = "Charging" ]; then
+        printf "^%s%%" "$CHARGE"
+    else
+        printf ".%s%%" "$CHARGE"
+    fi
+    printf "%s" "$SEP2"
+}
+
+dwm_battery
--- shared/config/Useful-Scripts/bar-functions/clock.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-dwm_clock (){
-  dte="$(date +"%l:%M%p | %B %d"| sed -e 's/^[ \t]*//')"
-  printf "%s" "$SEP1"
-  printf "%s" "$dte"
-  printf "%s" "$SEP2"
-}
-dwm_clock
