655ae36f8b46cb2390e33b61568828e8f8717444
Trent Nicholson <nicholsontrent@gmail.com>
2021-07-07 00:36:20 +1200

    fix(neovim): Fix not having a notification that a formatter isn't installed

--- configs/neovim/config/neovim/lua/sv/configs/formatter/utils.lua
+++ configs/neovim/config/neovim/lua/sv/configs/formatter/utils.lua
@@ -1,10 +1,19 @@
--- TODO: Find a way to check if the exe is installed
+local check_command_installed = function(command)
+    if os.execute('! which ' .. command .. ' &> /dev/null') == 0 then
+        print("You must install " .. command)
+        return false
+    end
+    return true
+end
+
 local prettierd = function()
+    if check_command_installed('prettierd') then return end
     local filename = vim.api.nvim_buf_get_name(0):gsub("([%[%]])", "%\\%1") -- escape only characters from set
     return {exe = "prettierd", args = {filename}, stdin = true}
 end
 
 local rustfmt = function()
+    if check_command_installed('rustfmt') then return end
     return {exe = "rustfmt", args = {"--emit=stdout"}, stdin = true}
 end
 
@@ -12,6 +21,7 @@ local gofmt = function() return {exe = "gofumpt", stdin = true} end
 
 -- TODO: Better options for this
 local luaformat = function()
+    if check_command_installed('lua-format') then return end
     return {exe = "lua-format -i --align-parameter --align-args", stdin = true}
 end
 
