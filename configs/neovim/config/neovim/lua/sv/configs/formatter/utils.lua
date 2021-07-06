031708ae19af1024ba4d93aba62d3e8898727d4c
Trent Nicholson <nicholsontrent@gmail.com>
2021-07-07 00:43:16 +1200

    fix(neovim): Fix formatters all failing

--- configs/neovim/config/neovim/lua/sv/configs/formatter/utils.lua
+++ configs/neovim/config/neovim/lua/sv/configs/formatter/utils.lua
@@ -7,21 +7,24 @@ local check_command_installed = function(command)
 end
 
 local prettierd = function()
-    if check_command_installed('prettierd') then return end
+    if not check_command_installed('prettierd') then return end
     local filename = vim.api.nvim_buf_get_name(0):gsub("([%[%]])", "%\\%1") -- escape only characters from set
     return {exe = "prettierd", args = {filename}, stdin = true}
 end
 
 local rustfmt = function()
-    if check_command_installed('rustfmt') then return end
+    if not check_command_installed('rustfmt') then return end
     return {exe = "rustfmt", args = {"--emit=stdout"}, stdin = true}
 end
 
-local gofmt = function() return {exe = "gofumpt", stdin = true} end
+local gofmt = function()
+    if not check_command_installed('gofumpt') then return end
+    return {exe = "gofumpt", stdin = true}
+end
 
 -- TODO: Better options for this
 local luaformat = function()
-    if check_command_installed('lua-format') then return end
+    if not check_command_installed('lua-format') then return end
     return {exe = "lua-format -i --align-parameter --align-args", stdin = true}
 end
 
