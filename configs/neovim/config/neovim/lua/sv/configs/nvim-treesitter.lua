adacb3ceb3336cc4ddc71207a113480af085c786
Trent Nicholson <nicholsontrent@gmail.com>
2022-04-07 09:44:21 +1200

    Remove textobject args from treesitter config as I removed the plugin

--- configs/neovim/config/neovim/lua/sv/configs/nvim-treesitter.lua
+++ configs/neovim/config/neovim/lua/sv/configs/nvim-treesitter.lua
@@ -16,18 +16,5 @@ require'nvim-treesitter.configs'.setup {
             "#b48ead"
         }
     },
-    highlight = {enable = true, additional_vim_regex_highlighting = false},
-    textobjects = {
-        select = {
-            enable = true,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner"
-
-            }
-        }
-    }
+    highlight = {enable = true, additional_vim_regex_highlighting = false}
 }
