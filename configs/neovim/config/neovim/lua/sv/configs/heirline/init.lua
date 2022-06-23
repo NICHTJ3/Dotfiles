3b1850148bbf159b6c43bd6e66f905ff409b1cb0
Trent Nicholson <nicholsontrent@gmail.com>
2022-06-23 12:23:11 +1200

    chore(neovim): Format heirline config incase I want to use it in the future

--- configs/neovim/config/neovim/lua/sv/configs/heirline/init.lua
+++ configs/neovim/config/neovim/lua/sv/configs/heirline/init.lua
@@ -7,22 +7,22 @@ local Align = Modules.Align
 
 local DefaultStatusline = {
     Modules.ViMode, Space, Modules.Spell, Modules.FileNameBlock, Space,
-    Modules.GitBranch, Modules.Git, {provider = "%<"}, Align, Align,
+    Modules.GitBranch, Modules.Git, { provider = "%<" }, Align, Align,
     Modules.LSPActive, Modules.LSPMessages, Space, Modules.Diagnostics, Space,
     Space, Space, Modules.ScrollPercentage, Space, Modules.ScrollBar
 }
 
 local InactiveStatusline = {
     condition = function() return not conditions.is_active() end,
-    {hl = {fg = Modules.sett.inactive_bkg, force = true}},
+    { hl = { fg = Modules.sett.inactive_bkg, force = true } },
     Modules.FileNameBlock,
-    {provider = "%<"},
+    { provider = "%<" },
     Align
 }
 
 local GitCommitStatusline = {
     condition = function()
-        return conditions.buffer_matches({filetype = {"^git.*", "fugitive"}})
+        return conditions.buffer_matches({ filetype = { "^git.*", "fugitive" } })
     end,
     Modules.ViMode,
     Space,
@@ -34,19 +34,22 @@ local GitCommitStatusline = {
 local SpecialStatusline = {
     condition = function()
         return conditions.buffer_matches(
-                   {buftype = {"nofile", "prompt", "help", "quickfix"}})
+            { buftype = { "nofile", "prompt", "help", "quickfix" } })
     end,
+    Modules.ViMode,
     Space,
     Modules.HelpFilename,
+    Space,
+    Modules.GitBranch,
     Align
 }
 
 local TerminalStatusline = {
     condition = function()
-        return conditions.buffer_matches({buftype = {"terminal"}})
+        return conditions.buffer_matches({ buftype = { "terminal" } })
     end,
-    hl = {bg = Modules.sett.curr_dir},
-    {condition = conditions.is_active, Modules.ViMode, Space},
+    hl = { bg = Modules.sett.curr_dir },
+    { condition = conditions.is_active, Modules.ViMode, Space },
     Space,
     Modules.TerminalName,
     Align
@@ -75,4 +78,5 @@ local StatusLines = {
     DefaultStatusline
 }
 
+vim.o.laststatus = 3
 require("heirline").setup(StatusLines)
