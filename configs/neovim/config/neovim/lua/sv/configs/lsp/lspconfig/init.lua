179994b07aa257085cff41ee3fef2989ddcc47ad
Trent Nicholson <nicholsontrent@gmail.com>
2022-07-11 10:30:14 +1200

    fixup! feat(lsp): support for eslint in pnp repo's (#23)

--- configs/neovim/config/neovim/lua/sv/configs/lsp/lspconfig/init.lua
+++ configs/neovim/config/neovim/lua/sv/configs/lsp/lspconfig/init.lua
@@ -8,8 +8,9 @@ local eslint = require("sv.configs.lsp.lspconfig.eslint")
 local tailwindcss = require('sv.configs.lsp.lspconfig.tailwindcss')
 local tsserver = require('sv.configs.lsp.lspconfig.tsserver')
 
-lspconfig.util.default_config.capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities())
+lspconfig.util.default_config.capabilities =
+    require('cmp_nvim_lsp').update_capabilities(
+        vim.lsp.protocol.make_client_capabilities())
 
 local M = {}
 
@@ -17,12 +18,27 @@ M.configs = {
     cssls = cssls.config,
     denols = denols.config,
     eslint = eslint.config,
-    graphql = { root_dir = lspconfig.util.root_pattern('.graphqlrc*', '.graphql.config.*', 'graphql.config.*') }
-    jsonls = { settings = { json = { schemas = require('schemastore').json.schemas() } } },
-    stylelint_lsp = { filetypes = { "css", "less", "scss", "sugarss", "wxss" }, on_new_config = function(config, new_root_dir) local stylelint_config = require("lspconfig.server_configurations.stylelint_lsp") local cmd = stylelint_config.default_config.cmd stylelint_config.default_config.on_new_config(config, new_root_dir) config.cmd = utils.get_cmd(cmd) end },
+    graphql = {
+        root_dir = lspconfig.util.root_pattern('.graphqlrc*',
+                                               '.graphql.config.*',
+                                               'graphql.config.*')
+    },
+    jsonls = {
+        settings = {json = {schemas = require('schemastore').json.schemas()}}
+    },
+    stylelint_lsp = {
+        filetypes = {"css", "less", "scss", "sugarss", "wxss"},
+        on_new_config = function(config, new_root_dir)
+            local stylelint_config = require(
+                                         "lspconfig.server_configurations.stylelint_lsp")
+            local cmd = stylelint_config.default_config.cmd
+            stylelint_config.default_config.on_new_config(config, new_root_dir)
+            config.cmd = utils.get_cmd(cmd)
+        end
+    },
     sumneko_lua = sumneko_lua.config,
     tailwindcss = tailwindcss.config,
-    tsserver = tsserver.config,
+    tsserver = tsserver.config
 }
 
 return M
