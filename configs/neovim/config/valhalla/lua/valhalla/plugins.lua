d601c97ea69b4dea222e4d583d6634f87a10868c
Trent Nicholson <nicholsontrent@gmail.com>
2022-08-15 11:31:22 +1200

    break(neovim): Switch to neo-tree from nvimtree

--- configs/neovim/config/valhalla/lua/valhalla/plugins.lua
+++ configs/neovim/config/valhalla/lua/valhalla/plugins.lua
@@ -185,12 +185,46 @@ local function spec(use)
 
     -- UI
     use {
-        "kyazdani42/nvim-tree.lua", -- File tree
-        requires = "kyazdani42/nvim-web-devicons",
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            {
+                "mrbjarksen/neo-tree-diagnostics.nvim",
+                module = "neo-tree.sources.diagnostics",
+            },
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            {
+                -- only needed if you want to use the commands with "_with_window_picker" suffix
+                "s1n7ax/nvim-window-picker",
+                tag = "v1.*",
+                config = function()
+                    require("window-picker").setup {
+                        autoselect_one = true,
+                        include_current = false,
+                        filter_rules = {
+                            -- filter using buffer options
+                            bo = {
+                                -- if the file type is one of following, the window will be ignored
+                                filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
+
+                                -- if the buffer type is one of following, the window will be ignored
+                                buftype = { "terminal" },
+                            },
+                        },
+                        other_win_hl_color = "#e35e4f",
+                    }
+                end,
+            },
+        },
         config = function()
-            require("valhalla.modules.nvimtree").setup()
+            local neotree = require "valhalla.modules.neotree"
+            neotree.setup()
+            neotree.keymaps()
         end,
     }
+
     use {
         "windwp/windline.nvim", -- Status line
         config = function()
