9459871c3c350920ca0792a6f757039e4b20d378
Trent Nicholson <nicholsontrent@gmail.com>
2022-09-16 21:20:04 +1200

    feat(lang): Add astro filetype

--- configs/neovim/config/valhalla/plugin/null-ls.lua
+++ configs/neovim/config/valhalla/plugin/null-ls.lua
@@ -5,11 +5,14 @@ end
 
 null_ls.setup {
     sources = {
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.prettierd.with({
+            extra_filetypes = { "astro" },
+        }),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.diagnostics.gitlint,
+        null_ls.builtins.diagnostics.markdownlint,
     },
     on_attach = require "valhalla.modules.lsp.on-attach",
 }
