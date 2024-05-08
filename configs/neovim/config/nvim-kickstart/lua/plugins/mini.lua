d937b365b120adebdee4e5bc37a7037f4681eb31
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-08 12:55:10 +1200

    break(nvim): Drop neo-tree and oil in favor of mini.files

--- configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
+++ configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
@@ -1,9 +1,75 @@
+local function custom_mini_files_keymaps(MiniFiles)
+  local function toggle_minifiles()
+    if not MiniFiles.close() then
+      MiniFiles.open()
+    end
+  end
+  vim.keymap.set('n', '\\', toggle_minifiles, { desc = 'Toggle minifiles' })
+  vim.keymap.set('n', '-', toggle_minifiles, { desc = 'Toggle minifiles' })
+
+  local show_dotfiles = false
+
+  local filter_show = function()
+    return true
+  end
+
+  local filter_hide = function(fs_entry)
+    return not vim.startswith(fs_entry.name, '.')
+  end
+
+  local toggle_dotfiles = function()
+    show_dotfiles = not show_dotfiles
+    local new_filter = show_dotfiles and filter_show or filter_hide
+    MiniFiles.refresh { content = { filter = new_filter } }
+  end
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesBufferCreate',
+    callback = function(args)
+      local buf_id = args.data.buf_id
+      -- Tweak left-hand side of mapping to your liking
+      vim.keymap.set('n', 'H', toggle_dotfiles, { buffer = buf_id })
+    end,
+  })
+end
+
 return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'sa', -- Add surrounding in Normal and Visual modes
+          delete = 'sd', -- Delete surrounding
+          find = 'sf', -- Find surrounding (to the right)
+          find_left = 'sF', -- Find surrounding (to the left)
+          highlight = 'sh', -- Highlight surrounding
+          replace = 'sr', -- Replace surrounding
+          update_n_lines = 'sn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+        n_lines = 500,
+      }
+
+      local MiniFiles = require 'mini.files'
+      custom_mini_files_keymaps(MiniFiles)
+      MiniFiles.setup {
+        mappings = {
+          close = '<Esc>',
+          go_in = 'l',
+          go_in_plus = '<CR>',
+          go_out_plus = 'h',
+          reset = '<BS>',
+          reveal_cwd = '@',
+          show_help = 'g?',
+          synchronize = '=',
+          trim_left = '<',
+          trim_right = '>',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
