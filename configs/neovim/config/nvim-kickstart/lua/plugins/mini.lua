87630d6f3e5bb95ffef2b7daf959cfc45ec69a81
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-15 11:51:12 +1200

    break(nvim): remove the file name from the status line

--- configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
+++ configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
@@ -53,7 +53,6 @@ return {
           highlight = 'sh', -- Highlight surrounding
           replace = 'sr', -- Replace surrounding
           update_n_lines = 'sn', -- Update `n_lines`
-
           suffix_last = 'l', -- Suffix to search with "prev" method
           suffix_next = 'n', -- Suffix to search with "next" method
         },
@@ -82,14 +81,18 @@ return {
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup { use_icons = vim.g.have_nerd_font, trim_left = 1 }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
+      end
+
+      statusline.section_filename = function()
+        -- Just use the plain file name without the path in statusline
+        return '%t'
       end
 
       -- ... and there is more!
