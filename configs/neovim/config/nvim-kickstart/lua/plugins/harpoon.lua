de49e01fea3dca147c7f3dc489f89e8a9868f544
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-15 11:51:46 +1200

    break/feat(nvim): Add harpoon. This replaces leader + sh mapping

---
+++ configs/neovim/config/nvim-kickstart/lua/plugins/harpoon.lua
@@ -0,0 +1,31 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Harpoon [A]dd' })
+
+    vim.keymap.set('n', '<leader>sh', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = '[S]how [H]arpoon' })
+
+    -- Meta + HJKL to navigate between Harpoon list
+    vim.keymap.set('n', '˙', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '∆', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '˚', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '¬', function()
+      harpoon:list():select(4)
+    end)
+  end,
+}
