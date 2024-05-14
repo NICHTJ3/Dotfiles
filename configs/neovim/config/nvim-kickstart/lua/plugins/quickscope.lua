981e8f7aca0e6b7c5f20960eff70852a8f96b125
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-15 11:50:31 +1200

    feat(nvim): Setup quickscope

---
+++ configs/neovim/config/nvim-kickstart/lua/plugins/quickscope.lua
@@ -0,0 +1,8 @@
+return {
+  'unblevable/quick-scope',
+  init = function()
+    vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    vim.g.qs_max_chars = 150
+    vim.g.qs_lazy_highlight = 1
+  end,
+}
