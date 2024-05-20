a772f07548018b745464cc9b92c436774112b6c8
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-21 11:31:44 +1200

    break(nvim): Change keymaps for mini.surround to not conflict with visual mode `s`

--- configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
+++ configs/neovim/config/nvim-kickstart/lua/plugins/mini.lua
@@ -5,12 +5,12 @@ return {
       require('mini.ai').setup { n_lines = 500 }
       require('mini.surround').setup {
         mappings = {
-          add = 'sa', -- Add surrounding in Normal and Visual modes
-          delete = 'sd', -- Delete surrounding
+          add = 'ys', -- Add surrounding in Normal and Visual modes. Carried over from the old surround.nvim plugin
+          delete = 'ds', -- Delete surrounding
           find = 'sf', -- Find surrounding (to the right)
           find_left = 'sF', -- Find surrounding (to the left)
           highlight = 'sh', -- Highlight surrounding
-          replace = 'sr', -- Replace surrounding
+          replace = 'cs', -- Change surrounding
           update_n_lines = 'sn', -- Update `n_lines`
           suffix_last = 'l', -- Suffix to search with "prev" method
           suffix_next = 'n', -- Suffix to search with "next" method
