0206d94fcb57d8fdf96184ecb0a8d5c59d6ae17f
Trent Nicholson <nicholsontrent@gmail.com>
2024-05-28 22:10:31 +1200

    !fix(nvim) Prevent tsserver formatting from running

--- configs/neovim/config/nvim-kickstart/lua/plugins/lspconfig.lua
+++ configs/neovim/config/nvim-kickstart/lua/plugins/lspconfig.lua
@@ -24,7 +24,12 @@ return {
       {
         'pmizio/typescript-tools.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {},
+        opts = {
+          on_attach = function(client)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
       },
       'Decodetalkers/csharpls-extended-lsp.nvim',
     },
