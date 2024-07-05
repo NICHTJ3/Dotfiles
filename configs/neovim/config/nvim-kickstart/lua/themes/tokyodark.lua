dd82a228ba5c9b00c287cbc3b4e05ca7f4d601fa
Trent Nicholson <nicholsontrent@gmail.com>
2024-07-05 13:12:29 +1200

    fix(nvim): Fix tokyonight theme not loading correctly if used

--- configs/neovim/config/nvim-kickstart/lua/themes/tokyodark.lua
+++ configs/neovim/config/nvim-kickstart/lua/themes/tokyodark.lua
@@ -1,5 +1,7 @@
 return {
   'tiagovla/tokyodark.nvim',
+  lazy = false,
+  priority = 1000,
   opts = {},
   config = function(_, opts)
     require('tokyodark').setup(opts) -- calling setup is optional
