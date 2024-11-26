64a91a630980a6177b4ca523942dcf768b7110b6
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-27 12:56:11 +1300

    feat(nvim/snacks): Use snacks for symbol highlighting and jumping

--- configs/neovim/config/uhoh/lua/plugins/snacks.lua
+++ configs/neovim/config/uhoh/lua/plugins/snacks.lua
@@ -6,7 +6,25 @@ return {
   opts = {
     quickfile = { enabled = true },
     bigfile = { enabled = true },
+    words = { enabled = true },
   },
-  keys = {},
+  keys = {
+    {
+      ']]',
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+      desc = 'Next Reference',
+      mode = { 'n', 't' },
+    },
+    {
+      '[[',
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+      desc = 'Prev Reference',
+      mode = { 'n', 't' },
+    },
+  },
   init = function() end,
 }
