9c10cb933c9f6a5f01ee352971d2717dda80e773
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-27 13:13:49 +1300

    feat(nvim/snacks): Setup nvim news

--- configs/neovim/config/uhoh/lua/plugins/snacks.lua
+++ configs/neovim/config/uhoh/lua/plugins/snacks.lua
@@ -67,6 +67,26 @@ return {
       end,
       desc = 'Git Browse',
     },
+
+    -- Nvim news!! OMG I've wanted this setup for so long and couldn't be bothered
+    {
+      "<leader>N",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = "yes",
+            statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
+    }
   },
   init = function() end,
 }
