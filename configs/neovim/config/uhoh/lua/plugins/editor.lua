7eeb9385413c0371545e3bfd73dd6ac15eafe4eb
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-28 11:58:42 +1300

    break(nvim): Remove 'nvim-autopairs'

--- configs/neovim/config/uhoh/lua/plugins/editor.lua
+++ configs/neovim/config/uhoh/lua/plugins/editor.lua
@@ -269,19 +269,6 @@ return {
       { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
   },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    -- Optional dependency
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-      require('nvim-autopairs').setup {}
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-  },
   { 'folke/ts-comments.nvim', opts = {}, event = 'VeryLazy' },
   {
     'jinh0/eyeliner.nvim',
