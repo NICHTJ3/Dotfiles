dda48733c5aeca002cc739d075aef4ca70330621
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-27 20:58:20 +1300

    feat(nvim/snacks): Setup `<leader>gc` git commit mapping

--- configs/neovim/config/uhoh/lua/plugins/snacks.lua
+++ configs/neovim/config/uhoh/lua/plugins/snacks.lua
@@ -69,6 +69,13 @@ return {
       end,
       desc = 'Git Browse',
     },
+    {
+      '<leader>gc',
+      function()
+        Snacks.terminal 'git commit'
+      end,
+      desc = 'Git commit',
+    },
 
     -- Nvim news!! OMG I've wanted this setup for so long and couldn't be bothered
     {
