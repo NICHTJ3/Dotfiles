bef1fea2a73705877989f96ddec9e133242601f7
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-27 13:42:58 +1300

    chore(nvim): Remove the old valhalla nvim config

--- configs/neovim/config/valhalla/lua/plugins/statusline.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
-        options = { theme = "auto", globalstatus = true },
-    },
-    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-}
--- configs/neovim/config/valhalla/lua/plugins/tmux.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "christoomey/vim-tmux-navigator",   -- Unifies tmux and vim navigation
-  "roxma/vim-tmux-clipboard",         -- Unifies vim panes in tmux sessions clipboards
-}
