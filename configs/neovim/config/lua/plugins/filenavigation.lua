0a6fc102ed6ad4f153f872e86fbfebded2a4afb1
Trent Nicholson <nicholsontrent@gmail.com>
2025-01-27 14:05:29 +1300

    feat(nvim/snacks)!: Replace telescope with snacks picker

--- configs/neovim/config/lua/plugins/filenavigation.lua
+++ configs/neovim/config/lua/plugins/filenavigation.lua
@@ -15,62 +15,6 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons' },
-    },
-    -- stylua: ignore
-    keys = {
-      -- local builtin = require rtelescope.builtin'
-      { '<leader>sr', function() require('telescope.builtin').resume() end, desc = '[S]earch [R]esume', },
-      { '<leader>sk', function() require('telescope.builtin').keymaps() end, desc = '[S]earch [K]eymaps', },
-      { '<leader>ss', function() require('telescope.builtin').builtin() end, desc = '[S]earch [S]elect Telescope', },
-      { '<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord', },
-      { '<leader>sg', function() require('telescope.builtin').live_grep() end, desc = '[S]earch by [G]rep', },
-      { '<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = '[S]earch [D]iagnostics', },
-      { '<leader>sf', function() require('telescope.builtin').find_files() end, desc = '[S]earch [F]iles', },
-      { '<leader><space>', function() require('telescope.builtin').find_files() end, desc = 'Search Files', },
-      { '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, }) end, desc = ' Fuzzily search in current buffer', },
-      { '<leader>uC', function() require('telescope.builtin').colorscheme { enable_preview = true } end, desc = 'Colorscheme with Preview', },
-      -- Shortcut for searching your Neovim configuration files
-      { '<leader>sn', function() require('telescope.builtin').find_files { follow = true, cwd = vim.fn.stdpath 'config', file_ignore_patterns = { 'spell', 'undodir', }, } end, desc = '[S]earch [N]eovim files', },
-    },
-    config = function()
-      require('telescope').setup {
-        pickers = {
-          find_files = {
-            find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
-          },
-          grep_string = {
-            additional_args = { '--hidden' },
-          },
-          live_grep = {
-            additional_args = { '--hidden' },
-          },
-        },
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-    end,
-  },
-  {
     'nvim-neo-tree/neo-tree.nvim',
     cmd = 'Neotree',
     keys = {
@@ -186,6 +130,32 @@ return {
       { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
       { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
+  {
+    'folke/snacks.nvim',
+    opts = { picker = {} },
+    -- stylua: ignore
+    keys={
+      { '<leader>sf', function() Snacks.picker.pick('files', { layout = 'vscode', }) end, desc = 'Snacks picker [S]earch [F]iles', },
+      { '<leader><space>', function() Snacks.picker.pick('files', { layout = 'vscode' }) end, desc = 'Snacks picker Search Files', },
+      { '<leader>uC', function() Snacks.picker.pick 'colorschemes' end, desc = 'Colorscheme with Preview', },
+      { '<leader>sg', function() Snacks.picker.pick 'grep' end, desc = '[S]each by [G]rep', },
+      { '<leader>sw', function() Snacks.picker.pick 'grep_word' end, desc = '[S]earch current [W]ord', },
+      { '<leader>sd', function() Snacks.picker.pick('diagnostics') end, desc = '[S]earch [D]iagnostics', },
+      {
+        '<leader>sn',
+        function()
+          Snacks.picker.pick('files', {
+            layout = 'vscode',
+            ---@diagnostic disable-next-line: assign-type-mismatch
+            cwd = vim.fn.stdpath 'config',
+            follow = true,
+            exclude={'spell'}
+          })
+        end,
+        desc = '[S]earch [N]eovim Config',
+      }
     },
   },
 }
