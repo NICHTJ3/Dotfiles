b376a11beb0b6463f1b6cf0ad99ab468ff168da0
Trent Nicholson <nicholsontrent@gmail.com>
2025-02-25 08:20:46 +1300

    fix(nvim/copilot): Fix copilot not working with blink.cmp

--- configs/neovim/config/lua/plugins/copilot.lua
+++ configs/neovim/config/lua/plugins/copilot.lua
@@ -22,5 +22,23 @@ return {
       },
     },
   },
-  { 'giuxtaposition/blink-cmp-copilot' },
+  {
+    'saghen/blink.cmp',
+    optional = true,
+    dependencies = { 'giuxtaposition/blink-cmp-copilot' },
+    opts = {
+      sources = {
+        default = { 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            -- kind = 'Copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
