61404bbe8e598bd9432968a424a0b7f8aebd13ad
Trent Nicholson <nicholsontrent@gmail.com>
2025-03-06 22:08:26 +1300

    feat(nvim/terraform): Add terraform validate linting

--- configs/neovim/config/lua/plugins/extras/lang/terraform.lua
+++ configs/neovim/config/lua/plugins/extras/lang/terraform.lua
@@ -26,4 +26,14 @@ return {
       },
     },
   },
+  {
+    'mfussenegger/nvim-lint',
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        terraform = { 'terraform_validate' },
+        tf = { 'terraform_validate' },
+      },
+    },
+  },
 }
