58bbeded0eaeb4add2e24f83cc9c166f98944da0
Trent Nicholson <nicholsontrent@gmail.com>
2025-03-09 14:24:48 +1300

    feat(nvim/blink): Setup avante autocomplete

--- configs/neovim/config/lua/plugins/copilot.lua
+++ configs/neovim/config/lua/plugins/copilot.lua
@@ -82,4 +82,20 @@ return {
       },
     },
   },
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'Kaiser-Yang/blink-cmp-avante' },
+    opts = {
+      sources = {
+        default = { 'avante' },
+        providers = {
+          avante = {
+            module = 'blink-cmp-avante',
+            name = 'Avante',
+            opts = {},
+          },
+        },
+      },
+    },
+  },
 }
