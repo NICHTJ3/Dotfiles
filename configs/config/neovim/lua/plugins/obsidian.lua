9c7f8f1fb5fd101d5859609bdf3cf6a48f2f315f
Trent Nicholson <nicholsontrent@gmail.com>
2025-04-15 09:48:26 +1200

    break(nvim/obsidian): Disable ui features of obsidian
    These are still conflicting with the markdown rendering plugin

--- configs/config/neovim/lua/plugins/obsidian.lua
+++ configs/config/neovim/lua/plugins/obsidian.lua
@@ -48,7 +48,7 @@ return {
         blink = true,
       },
       ui = {
-        enable = true, -- set to false to disable all additional syntax features
+        enable = false, -- set to false to disable all additional syntax features
         update_debounce = 200, -- update delay after a text change (in milliseconds)
         max_file_length = 5000, -- disable UI features for files with more than this many lines
       },
