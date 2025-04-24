da6789695e3f2ee958fc82c26005aad3ddbc136b
Trent Nicholson <nicholsontrent@gmail.com>
2025-04-24 19:30:53 +1200

    feat(nvim/gh_actions_ls): Setup github actions lsp

--- configs/config/neovim/lua/plugins/extras/lang/git.lua
+++ configs/config/neovim/lua/plugins/extras/lang/git.lua
@@ -32,5 +32,14 @@ return {
         },
       },
     },
+    {
+      -- Added until nvim-lspconfig merges: https://github.com/williamboman/mason-lspconfig.nvim/pull/506
+      'neovim/nvim-lspconfig',
+      opts = {
+        servers = {
+          gh_actions_ls = {},
+        },
+      },
+    },
   },
 }
