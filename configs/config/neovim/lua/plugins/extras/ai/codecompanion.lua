29b9bdfb9c59836dee690d223fb626b254a3b4cc
Trent Nicholson <nicholsontrent@gmail.com>
2025-04-27 17:39:30 +1200

    feat(nvim): Update CodeCompanion keybindings with new inline prompt and chat options
    - Added a new keybinding `<leader>ai` for inline AI prompts in visual mode, allowing users to input custom commands interactively.
    - Updated `<leader>ac` to toggle the AI chat interface.
    - Introduced `<leader>aC` for creating a new AI chat session.

--- configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
+++ configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
@@ -28,8 +28,23 @@ return {
   keys = {
     { '<leader>a', mode = { 'n', 'v' }, '', desc = '+[A]I' },
     { '<leader>af', mode = { 'n', 'v' }, '<cmd>CodeCompanion /fix<cr>', desc = '[A]I [F]ix' },
+    {
+      '<leader>ai',
+      mode = { 'v' },
+      function()
+        local user_input = vim.fn.input '[AI]: What would you like help with? '
+
+        if user_input and user_input ~= '' then
+          vim.cmd('CodeCompanion ' .. user_input)
+        else
+          print 'No input provided'
+        end
+      end,
+      desc = '[A]I [I]nline',
+    },
     { '<leader>ae', mode = { 'n', 'v' }, '<cmd>CodeCompanion /explain<cr>', desc = '[A]I [E]xplain' },
-    { '<leader>ac', mode = { 'n', 'v' }, '<cmd>CodeCompanionChat<cr>', desc = '[A]I [C]hat' },
+    { '<leader>ac', mode = { 'n', 'v' }, '<cmd>CodeCompanionChat Toggle<cr>', desc = '[A]I [C]hat toggle' },
+    { '<leader>aC', mode = { 'n', 'v' }, '<cmd>CodeCompanionChat<cr>', desc = '[A]I [C]reate a new chat' },
     { '<leader>aa', mode = { 'n', 'v' }, '<cmd>CodeCompanionAction<cr>', desc = '[A]I [A]ction' },
   },
   dependencies = {
