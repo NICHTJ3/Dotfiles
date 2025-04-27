f140ede1e1bb5cb69b07773837b2410bdfcd4878
Trent Nicholson <nicholsontrent@gmail.com>
2025-04-27 17:06:02 +1200

    feat(nvim/ai): Add codecompanion key maps

--- configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
+++ configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
@@ -1,7 +1,37 @@
 return {
   'olimorris/codecompanion.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    strategies = {
+      chat = {
+        keymaps = {
+          close = {
+            modes = {
+              n = 'q',
+            },
+            index = 3,
+            callback = 'keymaps.close',
+            description = 'Close Chat',
+          },
+          stop = {
+            modes = {
+              n = '<C-c',
+            },
+            index = 4,
+            callback = 'keymaps.stop',
+            description = 'Stop Request',
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    { '<leader>a', mode = { 'n', 'v' }, '', desc = '+[A]I' },
+    { '<leader>af', mode = { 'n', 'v' }, '<cmd>CodeCompanion /fix<cr>', desc = '[A]I [F]ix' },
+    { '<leader>ae', mode = { 'n', 'v' }, '<cmd>CodeCompanion /explain<cr>', desc = '[A]I [E]xplain' },
+    { '<leader>ac', mode = { 'n', 'v' }, '<cmd>CodeCompanionChat<cr>', desc = '[A]I [C]hat' },
+    { '<leader>aa', mode = { 'n', 'v' }, '<cmd>CodeCompanionAction<cr>', desc = '[A]I [A]ction' },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
