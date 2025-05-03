1dad0a42b4cd11b07c8361d8ffdbbfcc4fa14261
Trent Nicholson <nicholsontrent@gmail.com>
2025-05-03 21:04:30 +1200

    feat(nvim): Add rest language extra

--- configs/config/neovim/init.lua
+++ configs/config/neovim/init.lua
@@ -22,6 +22,7 @@ require('core.init').setup {
     { import = 'plugins.extras.lang.markdown' },
     { import = 'plugins.extras.lang.docker' },
     { import = 'plugins.extras.lang.css' },
+    { import = 'plugins.extras.lang.rest' },
   },
 }
 
