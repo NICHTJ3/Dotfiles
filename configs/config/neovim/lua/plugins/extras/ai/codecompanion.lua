ac0f70d7755f97700fa23de1c8dd7cdab514b04b
Trent Nicholson <nicholsontrent@gmail.com>
2025-10-05 13:32:48 +1300

    feat(nvim): Setup codecompanion memory

--- configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
+++ configs/config/neovim/lua/plugins/extras/ai/codecompanion.lua
@@ -20,6 +20,29 @@ return {
           provider = 'snacks',
         },
       },
+      memory = {
+        default = {
+          description = 'Collection of common files for all projects',
+          files = {
+            '.clinerules',
+            '.cursorrules',
+            '.goosehints',
+            '.rules',
+            '.windsurfrules',
+            '.github/copilot-instructions.md',
+            'AGENT.md',
+            'AGENTS.md',
+            { path = 'CLAUDE.md', parser = 'claude' },
+            { path = 'CLAUDE.local.md', parser = 'claude' },
+            { path = '~/.claude/CLAUDE.md', parser = 'claude' },
+          },
+        },
+        opts = {
+          chat = {
+            default_memory = 'default',
+          },
+        },
+      },
       strategies = {
         chat = {
           keymaps = {
