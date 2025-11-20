103be526a43c824c9210fd522fac1f1c8ed0917b
Trent Nicholson <nicholsontrent@gmail.com>
2025-11-21 01:00:16 +1300

    feat(nvim): Quickfix list navigation mappings

--- configs/config/neovim/lua/plugins/editor.lua
+++ configs/config/neovim/lua/plugins/editor.lua
@@ -251,6 +251,34 @@ return {
         end,
         desc = 'Next Trouble/Quickfix Item',
       },
+      {
+        '<Up>',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').prev { skip_groups = true, jump = true }
+          else
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = 'Previous Trouble/Quickfix Item',
+      },
+      {
+        '<Down>',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').next { skip_groups = true, jump = true }
+          else
+            local ok, err = pcall(vim.cmd.cnext)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = 'Next Trouble/Quickfix Item',
+      },
     },
   },
 
