87a508dfaec77c17209a4c6ff76a685e1b6c436c
Trent Nicholson <nicholsontrent@gmail.com>
2026-08-18 10:41:42 +1200

    perf(nvim)!: strip back roslyn lsp setup
    Roslyn nvim is causing some delays when quiting nvim. With this change
    we've moved to the plain lspconfig setup, but have lost some
    functionality

--- configs/config/neovim/lua/plugins/extras/lang/csharp/roslyn.lua
+++ configs/config/neovim/lua/plugins/extras/lang/csharp/roslyn.lua
@@ -52,60 +52,20 @@ return {
     },
   },
   {
-    'mason-org/mason.nvim',
-    opts = {
-      ensure_installed = { 'csharpier', 'netcoredbg', 'roslyn' },
-      registries = {
-        -- NOTE: Temporarily adding this registy, until https://github.com/mason-org/mason-registry/pull/6330 is merged
-        'github:Crashdummyy/mason-registry',
-      },
-    },
-  },
-  {
-    'seblyng/roslyn.nvim',
-    ft = {
-      'cs',
-      'razor',
-      'cshtml',
-    },
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-      filewatching = 'roslyn',
-    },
-  },
-  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        roslyn = {
-          settings = {
-            ['csharp|background_analysis'] = {
-              dotnet_analyzer_diagnostics_scope = 'fullSolution',
-              dotnet_compiler_diagnostics_scope = 'fullSolution',
-            },
-            ['csharp|code_lens'] = {
-              dotnet_enable_references_code_lens = true,
-            },
-            ['csharp|completion'] = {
-              dotnet_show_name_completion_suggestions = true,
-              dotnet_show_completion_items_from_unimported_namespaces = true,
-              dotnet_provide_regex_completions = true,
-            },
-            ['csharp|inlay_hints'] = {
-              csharp_enable_inlay_hints_for_implicit_object_creation = true,
-              csharp_enable_inlay_hints_for_implicit_variable_types = true,
-              csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-              csharp_enable_inlay_hints_for_types = true,
-              dotnet_enable_inlay_hints_for_indexer_parameters = true,
-              dotnet_enable_inlay_hints_for_literal_parameters = true,
-              dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-              dotnet_enable_inlay_hints_for_other_parameters = true,
-              dotnet_enable_inlay_hints_for_parameters = true,
+        roslyn_ls = {
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = false,
+              },
             },
           },
         },
       },
     },
   },
+  -- { 'seblyng/roslyn.nvim', }, -- TODO: I'd like to re-anable this, but the exit times are unbareable
 }
